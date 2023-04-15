5b7c33938da56759d7747b84471fe0a2e50fb9d9
Darek100100 <d.roza5959@gmail.com>
2023-04-15 23:34:52 +0200

    added getters and setters

--- src/main/java/com/example/demo/registration/RegistrationRequest.java
+++ src/main/java/com/example/demo/registration/RegistrationRequest.java
@@ -11,10 +11,37 @@ import lombok.*;
 
 public class RegistrationRequest {
 
-    private final String firstName;
-    private final String secondName;
-    private final String password;
-    private final String email;
+    private String firstName;
+    private String secondName;
+    private String password;
+    private String email;
 
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String value){
+        firstName = value;
+    }
+    public String getSecondName(){
+        return secondName;
+    }
+    public void setSecondName(String value){
+        secondName = value;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String value){
+        password = value;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String value){
+        email = value;
+    }
 
 }
+
+
+
