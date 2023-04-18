babc707691d474fb73715f8dc56163af5c6d45b0
Darek100100 <d.roza5959@gmail.com>
2023-04-18 14:38:57 +0200

    Implemented all claims of JetService

--- src/main/java/com/example/demo/config/JwtService.java
+++ src/main/java/com/example/demo/config/JwtService.java
@@ -1,7 +1,25 @@
 package com.example.demo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
+
 public class JwtService {
+
     public String extractUsername(String token) {
         return null;
     }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+
+
 }
